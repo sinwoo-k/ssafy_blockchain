a914ae47e0c1d18cec1b089799d2d19f011e81da
김도현 <kdhehsl@gmail.com>
2025-03-13 09:22:26 +0900

    🎉 Init: 백엔드 프로젝트 스켈레톤 코드

---
+++ BE/chaintoon/src/main/java/com/c109/chaintoon/ChaintoonApplication.java
@@ -0,0 +1,13 @@
+package com.c109.chaintoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaintoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaintoonApplication.class, args);
+	}
+
+}
