878255838e93b2d294a61dfdab66e21330512886
KimJiwon <rkl45687@gmail.com>
2025-03-17 17:24:10 +0900

    :sparkles: Feat: 웹툰 상세페이지 [S12P21C109](https://ssafy.atlassian.net/browse/S12P21C109-90) - 웹툰 정보 - 에피소드 리스트

--- FE/src/router/Router.jsx
+++ FE/src/router/Router.jsx
@@ -5,17 +5,27 @@ import Loader from '../components/common/Loader'
 
 // 웹툰 section
 const WebtoonMain = lazy(() => import('../pages/webtoon/WebtoonMain'))
+const WebtoonDetail = lazy(() => import('../pages/webtoon/WebtoonDetail'))
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootPage />,
     children: [
+      // 웹툰 관련
       {
         path: '',
         element: (
           <Suspense fallback={<Loader />}>
             <WebtoonMain />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/webtoon/:id',
+        element: (
+          <Suspense fallback={<Loader />}>
+            <WebtoonDetail />
           </Suspense>
         ),
       },
