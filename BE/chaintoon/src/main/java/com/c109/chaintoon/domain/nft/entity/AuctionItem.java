a01d2708a410aa747e5fb4650aacb3ab1934a0ce
허상운 <wdse123@naver.com>
2025-03-20 16:29:06 +0900

    ✨ Feat: 알림 API
    - [S12P21C109-197] 상회 입찰 알림 기능 구현
    - [S12P21C109-198] NFT 거래 알림 기능 구현
    - [S12P21C109-199] 2차 창작물 거래 알림 기능 구현

---
+++ BE/chaintoon/src/main/java/com/c109/chaintoon/domain/nft/entity/AuctionItem.java
@@ -0,0 +1,57 @@
+package com.c109.chaintoon.domain.nft.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "auction_item")
+public class AuctionItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auction_item_id")
+    private Integer auctionItemId;
+
+    @Column(name = "nft_id")
+    private Integer nftId;
+
+    @Column(name = "bidder_id")
+    private Integer bidderId;
+
+    @Column(name = "bidding_price")
+    private Double biddingPrice;
+
+    @Column(name = "buy_now_price")
+    private Double buyNowPrice;
+
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+    @Column(name = "ended", length = 1)
+    private String ended;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "success", length = 1)
+    private String success;
+
+    @PrePersist
+    protected void onCreate() {
+        startTime = LocalDateTime.now();
+        ended = "N";
+        createdAt = LocalDateTime.now();
+        success = "N";
+    }
+
+}
