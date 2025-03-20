a94ec797484db70c0bf4b34fd184a5627316d558
ruddms1522 <ruddms1522@naver.com>
2025-03-20 10:53:30 +0900

    🐛 Fix: UserIdNotFoundException NotFoundException 상속

--- BE/chaintoon/src/main/java/com/c109/chaintoon/domain/user/exception/UserIdNotFoundException.java
+++ BE/chaintoon/src/main/java/com/c109/chaintoon/domain/user/exception/UserIdNotFoundException.java
@@ -1,6 +1,8 @@
 package com.c109.chaintoon.domain.user.exception;
 
-public class UserIdNotFoundException extends RuntimeException {
+import com.c109.chaintoon.common.exception.NotFoundException;
+
+public class UserIdNotFoundException extends NotFoundException {
     public UserIdNotFoundException(Integer userId) {
         super( "해당 userId("+userId+ ")이 존재하지 않습니다.");
     }
