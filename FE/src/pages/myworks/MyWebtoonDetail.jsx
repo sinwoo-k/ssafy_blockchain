1b696d619642fe07c19d9da602480f55dcb3f2d2
KimJiwon <rkl45687@gmail.com>
2025-03-24 12:03:41 +0900

    :sparkles: Feat: 내 웹툰 상세 페이지 구현 [S12P21C109-217](https://ssafy.atlassian.net/browse/S12P21C109-217) - 내 웹툰 상세 페이지 - 내 웹툰 수정 페이지

---
+++ FE/src/pages/myworks/MyWebtoonDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import MyWebtoonDetailInfo from '../../components/myworks/MyWebtoonDetailInfo'
+import MyWebtoonDetailEpisodeList from '../../components/myworks/MyWebtoonDetailEpisodeList'
+
+const MyWebtoonDetail = () => {
+  const params = useParams()
+  return (
+    <div className='py-[60px]'>
+      <MyWebtoonDetailInfo webtoonId={params.id} />
+      <MyWebtoonDetailEpisodeList />
+    </div>
+  )
+}
+
+export default MyWebtoonDetail
