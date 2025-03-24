d87b11c6a694c1faa649e80fd501a311c70c43f4
KimJiwon <rkl45687@gmail.com>
2025-03-24 10:47:16 +0900

    :sparkles: Feat: 웹툰 등록 페이지 구현 [S12P21C109-93](https://ssafy.atlassian.net/browse/S12P21C109-93) - 폼 입력 - 대표이미지 crop

--- FE/src/pages/myworks/MyWebtoonCreate.jsx
+++ FE/src/pages/myworks/MyWebtoonCreate.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import MyWebtoonImageCropModal from '../../components/myworks/MyWebtoonImageCropModal'
 
 // 아이콘
 import CloseIcon from '@mui/icons-material/Close'
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
+import CheckBoxIcon from '@mui/icons-material/CheckBox'
 
 const MyWebtoonCreate = () => {
   const [webtoonName, setWebtoonName] = useState('') // 웹툰명
@@ -20,6 +23,14 @@ const MyWebtoonCreate = () => {
   const [genre, setGenre] = useState('') // 장르
   const [tags, setTags] = useState([]) // 태그
   const [summary, setSummary] = useState('') // 줄거리
+  const [garoImage, setGaroImage] = useState(null) // 가로형 이미지
+  const garoImageRef = useRef(null)
+  const [showGaroImageModal, setShowGaroImageModal] = useState(false)
+  const [seroImage, setSeroImage] = useState(null) // 세로형 이미지
+  const seroImageRef = useRef(null)
+  const [showSeroImageModal, setShowSeroImageModal] = useState(false)
+  const [adaptable, setAdaptable] = useState(false) // 팬아트 허용
+  const [confirmAgreement, setConfirmAgreement] = useState(false) // 약관동의
 
   // 태그 등록 함수
   const handleTags = (event) => {
@@ -37,9 +48,51 @@ const MyWebtoonCreate = () => {
     const temp = tags.filter((v, i) => v !== value || i !== index)
     setTags(temp)
   }
+  // 이미지 파일 선택시
+  const changeImageInput = (image, type) => {
+    if (type === 'garo') {
+      setGaroImage(image)
+      setShowGaroImageModal(true)
+      garoImageRef.current.value = ''
+    } else if (type === 'sero') {
+      setSeroImage(image)
+      setShowSeroImageModal(true)
+      seroImageRef.current.value = ''
+    }
+  }
+  // 이미지 input 열기
+  const handleImageInput = (type) => {
+    if (type === 'garo') {
+      garoImageRef.current.click()
+    } else if (type === 'sero') {
+      seroImageRef.current.click()
+    }
+  }
 
   const submitWebtoonForm = (event) => {
     event.preventDefault()
+    if (webtoonName.trim() === '') {
+      alert('웹툰명을 입력해주세요.')
+    } else if (genre === '') {
+      alert('장르를 선택해주세요.')
+    } else if (tags.length === 0) {
+      alert('태그를 1개 이상 입력해주세요.')
+    } else if (summary.trim() === '') {
+      alert('줄거리를 입력해주세요.')
+    } else if (garoImage === null || seroImage === null) {
+      alert('이미지를 등록해주세요.')
+    } else if (!confirmAgreement) {
+      alert('약관에 동의해주세요.')
+    } else {
+      const payload = {
+        webtoonName: webtoonName,
+        genre: genre,
+        tags: tags,
+        summary: summary,
+        adaptable: adaptable ? 'Y' : 'N',
+      }
+      console.log(payload)
+    }
   }
   return (
     <div className='flex justify-center py-[60px]'>
@@ -134,13 +187,14 @@ const MyWebtoonCreate = () => {
           {/* 줄거리 */}
           <div className='flex gap-3'>
             <label
-              htmlFor=''
+              htmlFor='summary-textarea'
               className='h-[48px] w-[135px] flex-none text-lg/[48px]'
             >
               줄거리
             </label>
             <div className='bg-text/50 border-chaintoon grow rounded-lg border p-3'>
               <textarea
+                id='summary-textarea'
                 className='placeholder:text-text/75 h-[150px] w-full resize-none focus:outline-none'
                 maxLength={500}
                 value={summary}
@@ -152,23 +206,130 @@ const MyWebtoonCreate = () => {
           </div>
           {/* 대표 이미지 */}
           <div className='flex gap-3'>
-            <label
-              htmlFor=''
-              className='h-[48px] w-[135px] flex-none text-lg/[44px]'
-            >
+            <label className='h-[48px] w-[135px] flex-none text-lg/[48px]'>
               대표 이미지
             </label>
+            <div className='flex gap-10'>
+              <div
+                className='bg-text/50 border-chaintoon h-[300px] w-[200px] overflow-hidden rounded-lg border'
+                onClick={() => handleImageInput('sero')}
+              >
+                {seroImage && (
+                  <img
+                    src={URL.createObjectURL(seroImage)}
+                    alt='세로 이미지'
+                    className='h-[300px] w-[200px]'
+                  />
+                )}
+                <input
+                  type='file'
+                  accept='image/jpeg, image/jpg'
+                  onChange={(event) =>
+                    changeImageInput(event.target.files[0], 'sero')
+                  }
+                  className='hidden'
+                  ref={seroImageRef}
+                />
+                {showSeroImageModal && (
+                  <MyWebtoonImageCropModal
+                    image={seroImage}
+                    setImage={setSeroImage}
+                    setShowModal={setShowSeroImageModal}
+                    type={'sero'}
+                  />
+                )}
+              </div>
+              <div
+                className='bg-text/50 border-chaintoon h-[300px] w-[400px] overflow-hidden rounded-lg border'
+                onClick={() => handleImageInput('garo')}
+              >
+                {garoImage && (
+                  <img
+                    src={URL.createObjectURL(garoImage)}
+                    alt='가로 이미지'
+                    className='h-[300px] w-[400px]'
+                  />
+                )}
+                <input
+                  type='file'
+                  accept='image/jpeg, image/jpg'
+                  onChange={(event) =>
+                    changeImageInput(event.target.files[0], 'garo')
+                  }
+                  className='hidden'
+                  ref={garoImageRef}
+                />
+                {showGaroImageModal && (
+                  <MyWebtoonImageCropModal
+                    image={garoImage}
+                    setImage={setGaroImage}
+                    setShowModal={setShowGaroImageModal}
+                    type={'garo'}
+                  />
+                )}
+              </div>
+            </div>
           </div>
           {/* 팬아트 허용 */}
           <div className='flex gap-3'>
-            <label
-              htmlFor=''
-              className='h-[48px] w-[135px] flex-none text-lg/[44px]'
-            >
+            <label className='h-[48px] w-[135px] flex-none text-lg/[48px]'>
               팬아트 허용
             </label>
+            <div className='flex h-[48px] items-center'>
+              {adaptable ? (
+                <div onClick={() => setAdaptable(false)}>
+                  <CheckBoxIcon
+                    sx={{ fontSize: 40 }}
+                    className='text-chaintoon cursor-pointer'
+                  />
+                </div>
+              ) : (
+                <div onClick={() => setAdaptable(true)}>
+                  <CheckBoxOutlineBlankIcon
+                    sx={{ fontSize: 40 }}
+                    className='cursor-pointer'
+                  />
+                </div>
+              )}
+            </div>
           </div>
           {/* 약관 동의 */}
+          <div>
+            <p>
+              저작권 등 다른 사람의 권리를 침해하거나 명예를 훼손하는 게시물은
+              이용약관 및 관련 법률에 의해 제재를 받으실 수 있습니다.
+              <br />
+              성인물, 폭력물 등의 게시물은 통보없이 삭제될 수 있습니다.
+            </p>
+            <div className='flex items-center gap-3'>
+              <div className='flex h-[48px] items-center'>
+                {confirmAgreement ? (
+                  <div onClick={() => setConfirmAgreement(false)}>
+                    <CheckBoxIcon
+                      sx={{ fontSize: 40 }}
+                      className='text-chaintoon cursor-pointer'
+                    />
+                  </div>
+                ) : (
+                  <div onClick={() => setConfirmAgreement(true)}>
+                    <CheckBoxOutlineBlankIcon
+                      sx={{ fontSize: 40 }}
+                      className='cursor-pointer'
+                    />
+                  </div>
+                )}
+              </div>
+              <p className='text-lg'>동의합니다.</p>
+            </div>
+          </div>
+          <div className='w-full'>
+            <button
+              className='bg-chaintoon h-[45px] w-full cursor-pointer rounded-lg text-black'
+              onClick={submitWebtoonForm}
+            >
+              등록하기
+            </button>
+          </div>
         </div>
       </div>
     </div>
