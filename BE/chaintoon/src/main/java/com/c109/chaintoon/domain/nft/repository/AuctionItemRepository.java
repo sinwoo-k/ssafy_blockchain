10d1e01583162a6d1443f0a188ff9b9a579928e5
김소연 <soyeon3589@naver.com>
2025-04-02 16:58:02 +0900

    🐛 Fix: 경매기능 오류 수정

--- BE/chaintoon/src/main/java/com/c109/chaintoon/domain/nft/repository/AuctionItemRepository.java
+++ BE/chaintoon/src/main/java/com/c109/chaintoon/domain/nft/repository/AuctionItemRepository.java
@@ -1,9 +1,15 @@
 package com.c109.chaintoon.domain.nft.repository;
 
 import com.c109.chaintoon.domain.nft.entity.AuctionItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuctionItemRepository extends JpaRepository<AuctionItem, Integer> {
+
+    Page<AuctionItem> findByTypeAndEnded(String type, String ended, Pageable pageable);
+
+    Page<AuctionItem> findByType(String type, Pageable pageable);
 }
