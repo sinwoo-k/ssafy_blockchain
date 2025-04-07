0b37d75a19762c19f0ae44a66128433284b4d0e2
김경은 <109902932+KyeongzowD@users.noreply.github.com>
2025-04-07 22:30:31 +0900

    🐛 Fix: 팔로우/팔로잉 리스트 조회 오류 수정

--- BE/chaintoon/src/main/java/com/c109/chaintoon/domain/user/repository/FolllowingRepository.java
+++ BE/chaintoon/src/main/java/com/c109/chaintoon/domain/user/repository/FolllowingRepository.java
@@ -13,11 +13,11 @@ import java.util.List;
 public interface FolllowingRepository extends JpaRepository<Following, FollowingId> {
 
     // 사용자(id)가 팔로우중인 사용자 목록 조회
-    @Query("SELECT f.id.followerId FROM Following f WHERE f.id.followerId=:userId")
+    @Query("SELECT f.id.followeeId FROM Following f WHERE f.id.followerId=:userId")
     List<Integer> findFollowingsById(@Param("userId") Integer id);
 
     // 사용자(id)를 팔로우중인 사용자 목록 조회
-    @Query("SELECT f.id.followeeId FROM Following f WHERE f.id.followeeId=:userId")
+    @Query("SELECT f.id.followerId FROM Following f WHERE f.id.followeeId=:userId")
     List<Integer> findFollowersById(@Param("userId") Integer id);
 
 }
