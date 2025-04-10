f644e1a48b3c6e0593fbf38a291541feb91ff8a0
허상운 <wdse123@naver.com>
2025-04-10 13:46:36 +0900

    🐛 Fix: 거래 내역 조회 시 거래 유저 닉네임 추가
    - 거래 내역 조회 시 거래 유저 닉네임 추가

--- BE/chaintoon/src/main/java/com/c109/chaintoon/domain/user/dto/response/TradingHistoryResponseDto.java
+++ BE/chaintoon/src/main/java/com/c109/chaintoon/domain/user/dto/response/TradingHistoryResponseDto.java
@@ -15,6 +15,7 @@ public class TradingHistoryResponseDto {
     private Double tradingValue;
     private String tradingDate;
     private String tradingTime;
-    private String otherParty; // 판매내역 : 구매자 정보, 구매내역 : 판매자 정보
+    private Integer otherPartyId; // 판매내역 : 구매자 정보, 구매내역 : 판매자 정보
+    private String otherPartyName;
     private String tradeType; // '판매한 것'인지 '구매한 것'인지 '거래중인 것'인지
 }
